c945e5c6f0db0d34b4f393a010923602206999c5
Hongzheng Wang <whzhyh@gmail.com>
2016-06-14 17:30:58 -0400

    [77. Combinations][Accepted]committed by LeetCode Extension

---
+++ 77-Combinations/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(n < 1 || k > n)
+            return ans;
+        List<Integer> tmp = new ArrayList<>();
+        dfs(ans, tmp, n, 1, k);
+        return ans;
+        
+    }
+    public void dfs(List<List<Integer>> ans, List<Integer> tmp, int n, int pos, int k) {
+        if(tmp.size() == k) {
+            ans.add(new ArrayList<Integer>(tmp));
+            return;
+        }
+        
+        for(int i = pos; i <= n; i++) {
+            tmp.add(i);
+            dfs(ans, tmp, n, i + 1, k);
+            tmp.remove(tmp.size() - 1);
+        }
+        
+    }
+    
+    
+}
