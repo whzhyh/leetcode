c338a722bd73b6b7aada81e42fda60d3f7c3369b
Hongzheng Wang <whzhyh@gmail.com>
2016-06-15 17:03:38 -0400

    [137. Single Number II][Accepted]committed by LeetCode Extension

--- 137-Single-Number-II/solution.java
+++ 137-Single-Number-II/solution.java
@@ -1,6 +1,15 @@
 public class Solution {
-    // General Method
+    // Best
     public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int i = 0; i < nums.length; i++){
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
+        }
+        return ones;
+    }
+    // General Method
+    public int singleNumber2(int[] nums) {
         if(nums == null || nums.length == 0)
             return - 1;
         int result = 0;
