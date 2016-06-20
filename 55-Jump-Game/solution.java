a80124425fa7d4e01b196d8940faf5c5672df570
Hongzheng Wang <whzhyh@gmail.com>
2016-06-20 01:44:39 -0400

    [55. Jump Game][Accepted]committed by LeetCode Extension

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,6 +1,22 @@
 public class Solution {
-    //Greedy
+    // DP
     public boolean canJump(int[] nums) {
+        boolean[] can = new boolean[nums.length];
+        can[0] = true;
+        
+        for(int i = 1; i < nums.length; i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                if(can[j] && j + nums[j] >= i) {
+                    can[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return can[nums.length - 1];
+    }
+    // Greedy
+    public boolean canJump2(int[] nums) {
         if(nums == null || nums.length == 0)
             return false;
             
