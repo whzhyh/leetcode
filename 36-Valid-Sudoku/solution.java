c797fd961abe339d8847676b7b9f1d7ec261524d
Hongzheng Wang <whzhyh@gmail.com>
2016-07-04 19:11:41 -0400

    [36. Valid Sudoku][Accepted]committed by LeetCode Extension

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -8,7 +8,7 @@ public class Solution {
             row = new HashSet<>();
             column = new HashSet<>();
             square = new HashSet<>();
-            System.out.println();
+            
             for(int j = 0; j < 9; j++) {
                 
                 if(board[i][j] != '.') {
@@ -29,10 +29,8 @@ public class Solution {
                     }    
                 }
                 
-                // System.out.println((i/3*3 + j/3) + " : " + (j%3 + i%3*3));
                 if(board[i/3*3 + j/3][j%3 + i%3*3] != '.') {  
                     int c = board[i/3*3 + j/3][j%3 + i%3*3] - '0';
-                    
                     if(square.contains(c)) {
                         return false;
                     } else {
