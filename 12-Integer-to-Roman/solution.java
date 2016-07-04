292e0ca092cc1bd2b113d9c11f75f1f8ac84197c
Hongzheng Wang <whzhyh@gmail.com>
2016-07-03 23:47:09 -0400

    [12. Integer to Roman][Accepted]committed by LeetCode Extension

--- 12-Integer-to-Roman/solution.java
+++ 12-Integer-to-Roman/solution.java
@@ -1,5 +1,18 @@
 public class Solution {
-    public String intToRoman(int num) {
+    public String intToRoman(int num) { 
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};    
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; num > 0; i++) {
+            while(num >= values[i]) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+    
+    public String intToRoman2(int num) {
         String[] I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
         String[] X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         String[] C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
