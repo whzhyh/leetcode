c9f45661d1b1f0753c1d6323d84f87d5345c8e5f
Hongzheng Wang <whzhyh@gmail.com>
2016-07-03 23:10:58 -0400

    [29. Divide Two Integers][Accepted]committed by LeetCode Extension

--- 29-Divide-Two-Integers/solution.java
+++ 29-Divide-Two-Integers/solution.java
@@ -1,15 +1,41 @@
 public class Solution {
-    public int divide(int dividend, int divisor) {
-        if(divisor==0) return Integer.MAX_VALUE;
+    
+    public int divide(int dividend, int divisor) { 
+        if(divisor == 0) return Integer.MAX_VALUE;
         
-        if(divisor==-1 && dividend == Integer.MIN_VALUE)
+        if(divisor == -1 && dividend == Integer.MIN_VALUE)
+            return Integer.MAX_VALUE;
+        
+        boolean isNegative = (dividend > 0) ^ (divisor > 0);
+        
+        long a = Math.abs((long)dividend);
+        long b = Math.abs((long)divisor);
+        int result = 0;
+        
+        while(a >= b) {
+            int shift = 0;
+            while(a >= (b << shift)) {
+                shift++;
+            }
+            a -= b << (shift - 1);
+            result += 1 << (shift - 1);
+        }
+        
+        return isNegative ? -result : result;
+    }
+    
+    // Binary Search
+    public int divide2(int dividend, int divisor) {
+        if(divisor == 0) return Integer.MAX_VALUE;
+        
+        if(divisor == -1 && dividend == Integer.MIN_VALUE)
             return Integer.MAX_VALUE;
             
         boolean isNegative = (dividend > 0) ^ (divisor > 0);
         
         long a = Math.abs((long)dividend);
         long b = Math.abs((long)divisor);
-        // System.out.println(b);
+        
         long start = 0, end = a;
         int result = 0;
         
