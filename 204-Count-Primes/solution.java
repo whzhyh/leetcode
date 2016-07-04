fe1b61119fa47ba4e11bad9170c676b6914f58e2
Hongzheng Wang <whzhyh@gmail.com>
2016-07-04 17:21:09 -0400

    [204. Count Primes][Accepted]committed by LeetCode Extension

--- 204-Count-Primes/solution.java
+++ 204-Count-Primes/solution.java
@@ -1,21 +1,23 @@
 public class Solution {
     public int countPrimes(int n) {
+        if(n < 2)
+            return 0;
+            
         boolean[] flags = new boolean[n];
         Arrays.fill(flags, true);
-        int result = 0;
+        int result = n - 2;
         
         for(int i = 2; i < n; i++) {
             if(flags[i] == false)
                 continue;
             for(int j = 2; i * j < n; j++) {
+                if(flags[i * j] == false)
+                    continue;
                 flags[i * j] = false;
+                result--;
             }
         }
         
-        for(int i = 2; i < n; i++) {
-            if(flags[i] == true)
-                result++;
-        }
         return result;
         
     }
