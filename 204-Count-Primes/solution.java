e5b541ca041ee3b608429821ab843cf408e4171f
Hongzheng Wang <whzhyh@gmail.com>
2016-07-04 17:34:23 -0400

    [204. Count Primes][Accepted]committed by LeetCode Extension

--- 204-Count-Primes/solution.java
+++ 204-Count-Primes/solution.java
@@ -1,25 +1,48 @@
 public class Solution {
+    
+    // Traditional method: TLE
+    public int countPrimes2(int n) {
+        int result = 0;
+        for(int i = 2; i < n; i++) {
+            if(isPrime(i))
+                result++;
+        }
+        return result;
+    }
+    
+    private boolean isPrime(int n) {
+        if(n < 2) return false;
+        
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    // Sieve of Eratosthenes
     public int countPrimes(int n) {
         if(n < 2)
             return 0;
             
-        boolean[] flags = new boolean[n];
-        Arrays.fill(flags, true);
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
         int result = n - 2;
         
         for(int i = 2; i < n; i++) {
-            if(flags[i] == false)
+            if(isPrime[i] == false) {
                 continue;
+            }
+            
             for(int j = 2; i * j < n; j++) {
-                if(flags[i * j] == false)
+                if(isPrime[i * j] == false)
                     continue;
-                flags[i * j] = false;
+                isPrime[i * j] = false;
                 result--;
             }
         }
         
         return result;
-        
     }
     
     
