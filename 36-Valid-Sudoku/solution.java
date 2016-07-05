0d6a8e9d1ff16d3c761005fbf9f2ec10f1b8f669
Hongzheng Wang <whzhyh@gmail.com>
2016-07-05 17:28:12 -0400

    [36. Valid Sudoku][Accepted]committed by LeetCode Extension

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -1,45 +1,46 @@
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<Character> row = new HashSet<>();
-        Set<Character> column = new HashSet<>();
-        Set<Character> square = new HashSet<>();
+        if(board == null || board.length == 0)
+            return false;
+            
+        int m = board.length, n = board[0].length;
         
-        for(int i = 0; i < 9; i++) {
+        if(m != n)
+            return false;
+            
+        HashSet<Character> row = new HashSet<Character>();
+        HashSet<Character> column = new HashSet<Character>();
+        HashSet<Character> cube = new HashSet<Character>();
+        
+        for(int i = 0; i < m; i++) {
             row.clear();
             column.clear();
-            square.clear();
-            
-            for(int j = 0; j < 9; j++) {
-                
+            cube.clear();
+            for(int j = 0; j < n; j++) {
                 if(board[i][j] != '.') {
-                    char a = board[i][j];
-                    if(row.contains(a)) {
+                    if(row.contains(board[i][j]))
                         return false;
-                    } else {
-                        row.add(a);
-                    }    
+                    else
+                        row.add(board[i][j]);
                 }
                 
-                if(board[j][i] != '.') { 
-                    char b = board[j][i];
-                    if(column.contains(b)) {
+                if(board[j][i] != '.') {
+                    if(column.contains(board[j][i]))
                         return false;
-                    } else {
-                        column.add(b);
-                    }    
+                    else
+                        column.add(board[j][i]);
                 }
                 
-                if(board[i/3*3 + j/3][i%3*3 + j%3] != '.') {  
-                    char c = board[i/3*3 + j/3][i%3*3 + j%3];
-                    if(square.contains(c)) {
+                char c = board[i/3*3 + j/3][i%3*3 + j%3];
+                if(c != '.') {
+                    if(cube.contains(c))
                         return false;
-                    } else {
-                        square.add(c);
-                    }
+                    else
+                        cube.add(c);
                 }
-                
             }
         }
+        
         return true;
     }
 }
