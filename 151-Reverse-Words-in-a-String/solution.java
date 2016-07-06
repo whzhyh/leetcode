65a1e97f5928d3ba2b601665e0de342f0aadc384
Hongzheng Wang <whzhyh@gmail.com>
2016-07-06 03:32:10 -0400

    [151. Reverse Words in a String][Accepted]committed by LeetCode Extension

---
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        int len = s.length();
+        if(len == 0)
+            return s;
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++) {
+            char start = s.charAt(i);
+            if(start == ' ')
+                continue;
+            int j = i;
+            for(; j < s.length(); j++) {
+                char end = s.charAt(j);
+                if(end == ' ')
+                    break;
+            }
+            list.add(s.substring(i, j));
+            i = j;
+        }
+        
+        int start = 0, end = list.size() - 1;
+        while(start < end) {
+            String tmp = list.get(start);
+            list.set(start, list.get(end));
+            list.set(end, tmp);
+            start++;
+            end--;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String str : list) {
+            
+            sb.append(str);
+            sb.append(" ");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+}
