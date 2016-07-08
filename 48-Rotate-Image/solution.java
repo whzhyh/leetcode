d338bb0ac455ebbc698eb734db9d22bb617d28ac
Hongzheng Wang <whzhyh@gmail.com>
2016-07-07 21:53:48 -0400

    [48. Rotate Image][Accepted]committed by LeetCode Extension

--- 48-Rotate-Image/solution.java
+++ 48-Rotate-Image/solution.java
@@ -3,13 +3,6 @@ public class Solution {
         if(matrix == null || matrix.length == 0)
             return;
         int n = matrix.length;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; i + j < n; j++) {
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[n - j - 1][n - i - 1];
-                matrix[n - j - 1][n - i - 1] = tmp;
-            }
-        }
         
         for(int i = 0; i < n / 2; i++) {
             for(int j = 0; j < n; j++) {
@@ -18,5 +11,15 @@ public class Solution {
                 matrix[n - i - 1][j] = tmp;
             }
         }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        
+        
     }
 }
