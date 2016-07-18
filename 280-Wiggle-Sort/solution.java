88b68f9f787f83fb3e5997587cfb2c53bebb2598
Hongzheng Wang <whzhyh@gmail.com>
2016-07-18 19:08:36 -0400

    [280. Wiggle Sort][Accepted]committed by LeetCode Extension

--- 280-Wiggle-Sort/solution.java
+++ 280-Wiggle-Sort/solution.java
@@ -3,13 +3,10 @@ public class Solution {
         for(int i = 1; i < nums.length; i++) {
             if((i % 2 == 1 && nums[i] < nums[i - 1]) ||
                 (i % 2 == 0 && nums[i] > nums[i - 1])) {
-                    swap(nums, i, i - 1);
+                    int tmp = nums[i];
+                    nums[i] = nums[i - 1];
+                    nums[i - 1] = tmp;
                 }
         }
-    }
-    public void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
     }
 }
