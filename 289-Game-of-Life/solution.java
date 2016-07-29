e31fb7e7a0d1acd63314bb3117128d26401126c4
Hongzheng Wang <whzhyh@gmail.com>
2016-07-28 20:22:52 -0400

    [289. Game of Life][Accepted]committed by LeetCode Extension

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -1,33 +1,44 @@
 public class Solution {
+    // four states: 0 -> 0, 0 -> 1, 1 -> 0, 1 -> 1
     public void gameOfLife(int[][] board) {
-        if(board == null || board.length == 0)
+        if(board == null || board.length == 0 || board[0] == null || board[0].length == 0)
             return;
-        int m = board.length, n = board[0].length;
+        int m = board.length;
+        int n = board[0].length;
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int lives = liveNeighbors(board, m, n, i, j);
-                if(board[i][j] == 1 && lives >= 2 && lives <= 3)
-                    board[i][j] = 3;
-                    
-                if(board[i][j] == 0 && lives == 3)
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int lives = neighborsLives(board, i, j);
+                if(board[i][j] == 0 && lives == 3) {
                     board[i][j] = 2;
+                }
+                if(board[i][j] == 1) {
+                    if(lives < 2 || lives > 3)
+                        board[i][j] = 1;
+                    if(lives >= 2 && lives <= 3)
+                        board[i][j] = 3;
+                }
             }
         }
         for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                board[i][j] = board[i][j] >> 1;
+            for(int j = 0; j < n; j++) { 
+                board[i][j] = (board[i][j] & 2) >> 1;   
             }
         }
+        
     }
-    private int liveNeighbors(int[][] board, int m, int n, int i, int j) {
-        int lives = 0;
-        for(int x = Math.max(i - 1, 0); x <= Math.min(i + 1, m - 1); x++) {
-            for(int y = Math.max(j - 1, 0); y <= Math.min(j + 1, n - 1); y++) {
-                lives += board[x][y] & 1;
+    public int neighborsLives(int[][] board, int x, int y) {
+        int m = board.length;
+        int n = board[0].length;
+        int count = 0;
+        for(int i = Math.max(0, x - 1); i <= Math.min(m - 1, x + 1); i++) {
+            for(int j = Math.max(0, y - 1); j <= Math.min(n - 1, y + 1); j++) {
+                if(i == x && j == y)
+                    continue;
+                if((board[i][j] & 1) == 1) 
+                    count++;
             }
         }
-        lives -= board[i][j] & 1;
-        return lives;
+        return count;
     }
 }
