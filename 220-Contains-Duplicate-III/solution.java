6542f7981ba669db5b62ad945453815f89c1f106
Hongzheng Wang <whzhyh@gmail.com>
2016-07-29 17:00:52 -0400

    [220. Contains Duplicate III][Accepted]committed by LeetCode Extension

---
+++ 220-Contains-Duplicate-III/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if(nums.length < 2 || k <= 0 || t < 0)
+            return false;
+        
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            if(set.floor(n) != null && n <= t + set.floor(n) ||
+            set.ceiling(n) != null && set.ceiling(n) <= t + n)
+                return true;
+            set.add(n);
+            if(i >= k)
+                set.remove(nums[i - k]);
+        }
+        return false;
+        
+    }
+}
