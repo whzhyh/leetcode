760ffb9ffc85857efe1bf932c704a871dd8a41ff
Hongzheng Wang <whzhyh@gmail.com>
2016-07-28 20:00:10 -0400

    [73. Set Matrix Zeroes][Accepted]committed by LeetCode Extension

--- 73-Set-Matrix-Zeroes/solution.java
+++ 73-Set-Matrix-Zeroes/solution.java
@@ -1,4 +1,5 @@
 public class Solution {
+    // My idea is simple: store states of each row in the first of that row, and store states of each column in the first of that column. Because the state of row0 and the state of column0 would occupy the same cell, I let it be the state of row0, and use another variable "col0" for column0. In the first phase, use matrix elements to set states in a top-down way. In the second phase, use states to set matrix elements in a bottom-up way.
     public void setZeroes(int[][] matrix) {
         if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0)
             return;
