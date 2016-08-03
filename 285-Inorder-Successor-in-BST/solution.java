c1725c6bfb82b5f56686e86fa1789009044c7a8f
Hongzheng Wang <whzhyh@gmail.com>
2016-08-02 20:29:44 -0400

    [285. Inorder Successor in BST][Accepted]committed by LeetCode Extension

---
+++ 285-Inorder-Successor-in-BST/solution.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        // write your code here
+        if(root == null)
+            return null;
+            
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode left = root;
+        while(left != null) {
+            stack.push(left);
+            left = left.left;
+        }
+        boolean isNext = false;
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if(isNext)
+                return node;
+            if(node.val == p.val) {
+                isNext = true;
+            }
+            
+            TreeNode l = node.right;
+            while(l != null) {
+                stack.push(l);    
+                l = l.left;
+            }
+        }
+        return null;
+            
+    }
+    
+    TreeNode ans = null;
+    public TreeNode inorderSuccessor2(TreeNode root, TreeNode p) {
+        if(root == null)
+            return root;
+        List<TreeNode> list = new ArrayList<>();
+        inorder(root, list, p);
+        return ans;
+    }
+    public void inorder(TreeNode root, List<TreeNode> list, TreeNode p) {
+        if(root == null || ans != null)
+            return;
+        inorder(root.left, list, p);
+        
+        if(list.size() > 0 && p == list.get(list.size() - 1)) {
+            ans = root;
+        }
+    
+        list.add(root);
+        inorder(root.right, list, p);
+    }
+}
