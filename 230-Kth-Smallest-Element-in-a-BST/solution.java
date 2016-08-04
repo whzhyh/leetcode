2dcebf3294aaa26780e1b8008c1f73d0197cc425
Hongzheng Wang <whzhyh@gmail.com>
2016-08-03 20:18:53 -0400

    [230. Kth Smallest Element in a BST][Accepted]committed by LeetCode Extension

---
+++ 230-Kth-Smallest-Element-in-a-BST/solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        return list.get(k-1);
+    }
+    public void inorder(TreeNode root, List<Integer> list) {
+        if(root == null)
+            return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+}
